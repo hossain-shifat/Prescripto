7b7736d9722a0c8b3f350623697637495a3a629a
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2026-01-31 16:53:40 +0600

    Fix logo component

--- components/frontend/Logo.jsx
+++ components/frontend/Logo.jsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 
 export default function Logo() {
     return (
-        <Link href='/'>
-            <Image src={assets.logo} width={200} className="logo-adaptive lg:px-5 w-35 lg:w-45" alt="prescripto logo"/>
+        <Link href='/' className="flex items-center gap-3">
+            <Image src={assets.logo} width={200} className="w-7 h-7 object-cover lg:w-8 lg:h-8" alt="prescripto logo"/>
+            <h1 className="font-bold text-xl md:text-2xl bg-linear-to-r from-[#5F6FFF] to-[#5F6FFF]/80 bg-clip-text text-transparent">
+                Prescripto
+            </h1>
         </Link>
     )
 }
